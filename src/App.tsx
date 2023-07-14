7f8bd7e96745ce3d4ef1cbdf50faf617e28d546b
wiliamfrisonribeiro <wiliamfrisonribeiro@gmail.com>
2023-07-14 18:17:36 -0300

    page header created

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
+
+import { Header } from './components/Header';
+import { GlobalStyle } from './styles/global';
+
 export function App() {
   return (
-    <div className="App">
-    <h1>Hello World</h1>
-    </div>
+    <>
+      <Header />
+      <GlobalStyle />
+    </>
   );
 }
 
